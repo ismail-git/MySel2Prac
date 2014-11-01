d1c81452e31c15d5cd5382d4bcb0cfa4cce2eccd
ismail <ismail.osmanjan@gmail.com>
2014-11-01 19:56:59 -0400

    Maven is build to integrate with Jenkins

--- WebAppTest/src/test/java/SelTesting/webTesting.java
+++ WebAppTest/src/test/java/SelTesting/webTesting.java
@@ -3,10 +3,14 @@ package SelTesting;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -17,21 +21,53 @@ public class webTesting {
     public WebDriver driver = new FirefoxDriver();
 
     @BeforeTest
+    @Parameters({"useSauceLabs", "userName", "access_Key", "os", "browser", "browserVersion", "url"})
+    public void setUp(boolean useSauceLabs, String userName, String access_Key, String os, String browser, String browserVersion, String url) throws Exception {
+        try {
+            if (useSauceLabs == true) {
+                SauceLabSetUp(os, browser, browserVersion, userName, access_Key);
+            } else {
+                if (browser.equalsIgnoreCase("firefox")) {
+                    driver = new FirefoxDriver();
+                }
+            }
 
-    public void setUp() throws Exception {
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.navigate().to("http://yahoo.com");
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            driver.navigate().to(url);
+
+        } catch (Exception e) {
+            throw (e);
+        }
     }
 
     @Test
     public void Test() {
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.manage().window().maximize();
 
     }
 
 
     @AfterTest
     public void tearDown() throws Exception {
-        driver.close();
+        driver.quit();
+    }
+
+    public void SauceLabSetUp(String os, String browser, String browserVersion, String userName, String access_Key)
+            throws Exception {
+        try {
+            // Choose the browser, version, and platform to test
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            capabilities.setCapability("version", browserVersion);
+            capabilities.setCapability("platform", os);
+
+            // Create the connection to SauceLabs to run the tests
+            this.driver = new RemoteWebDriver(
+                    new URL("http://" + userName + ":" + access_Key + "@ondemand.saucelabs.com:80/wd/hub"),
+                    capabilities);
+        } catch (Exception e) {
+            throw (e);
+        }
     }
 }
+
